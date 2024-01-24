1b63ae653bdb2d6aaa824cef330c9aab2e56b38f
mrolaleyeisrael <mrolaleyeisrael@gmail.com>
2024-01-24 13:10:41 +0100

    profile navigation completed

--- app/components/Profile.tsx
+++ app/components/Profile.tsx
@@ -5,15 +5,15 @@ import { arrowDown } from '../assets'
 
 const Profile = () => {
   return (
-    <div className='px-2 py-[6px] border flex items-center gap-2 rounded-full '>
+    <div className='px-2 py-[6px] border flex items-center gap-2 rounded-full cursor-pointer '>
 
       <Image src={userImage} alt='Account Image' className=' rounded-full h-[38px] w-[38px] ' />
 
       <div className=' flex flex-col gap-0'>
-        <h1 className=' mb-0'>Israel Olaleye</h1>
+        <h1 className=' font-semibold mb-0'>Israel Olaleye</h1>
         <p className=' text-sm '>mrolaleyeisrael@gmail.com</p>
       </div>
-      <Image src={arrowDown} alt='arrow down' className='' />
+      <Image src={arrowDown} alt='arrow down' className=' cursor-pointer' />
     </div>
   )
 }
